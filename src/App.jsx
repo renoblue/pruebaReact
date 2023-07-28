8593da47a01ae504777f942da0133f8f36a91c02
renoBlack <renato_chc9@hotmail.es>
2023-07-27 20:48:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Shoping Cart</h1>
+  );
+}
+
+export default App;
---
+++ src/util/data.js
@@ -0,0 +1,209 @@
+{
+  "productos":[
+    {
+      "caprId": 12,
+      "caprCodigo": 12,
+      "caprNombre": "Bienes de Control",
+      "caprNombreRuta": "Bienes de Control",
+      "caprPadre": "0",
+      "caprStatus": "1",
+      "catalogoProd": [
+      ]
+    },
+    {
+      "caprId": 13,
+      "caprCodigo": 13,
+      "caprNombre": "Terrenos",
+      "caprNombreRuta": "Bienes de Control / Terrenos",
+      "caprPadre": "12",
+      "caprStatus": "1",
+      "catalogoProd": [
+      ]
+    },
+    {
+      "caprId": 14,
+      "caprCodigo": 14,
+      "caprNombre": "Equipo de Oficina",
+      "caprNombreRuta": "Bienes de Control / Equipo de Oficina",
+      "caprPadre": "12",
+      "caprStatus": "1",
+      "catalogoProd": [
+      ]
+    },
+    {
+      "caprId": 25,
+      "caprCodigo": 29,
+      "caprNombre": "Abonos Orgánicos",
+      "caprNombreRuta": "Productos Agrícolas / Abonos Orgánicos",
+      "caprPadre": "28",
+      "caprStatus": "1",
+      "catalogoProd": [
+        {
+          "cproId": 1,
+          "cproCodigo": 183,
+          "caprId": 25,
+          "unidCodigo": 50,
+          "cproCodigoint": "SBA00210",
+          "cproCodigobarras": " ",
+          "cproNombre": "BIOCOMPOST X 23 KG",
+          "cproDescripcion": "BIOCOMPOST X 23 KG",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 2,
+          "cproCodigo": 184,
+          "caprId": 25,
+          "unidCodigo": 50,
+          "cproCodigoint": "SBA00006",
+          "cproCodigobarras": " ",
+          "cproNombre": "BIOWAY SACO X 23 KG",
+          "cproDescripcion": "BIOWAY SACO X 23 KG",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 3,
+          "cproCodigo": 185,
+          "caprId": 25,
+          "unidCodigo": 50,
+          "cproCodigoint": "SBA00003",
+          "cproCodigobarras": "",
+          "cproNombre": "ECOABONAZA_SACO X 23 KG",
+          "cproDescripcion": "ECOABONAZA_SACO X 23 KG",
+          "cproMarca": "0",
+          "cproUbicacion": "0",
+          "cproTipoPrecio": 2
+        }
+      ]
+    },
+    {
+      "caprId": 62,
+      "caprCodigo": 66,
+      "caprNombre": "Salud animal",
+      "caprNombreRuta": "Productos Pecuarios / Salud animal",
+      "caprPadre": "42",
+      "caprStatus": "1",
+      "catalogoProd": [
+      ]
+    },
+    {
+      "caprId": 63,
+      "caprCodigo": 67,
+      "caprNombre": "Analgésicos",
+      "caprNombreRuta": "Productos Pecuarios / Salud animal / Analgésicos",
+      "caprPadre": "66",
+      "caprStatus": "1",
+      "catalogoProd": [
+        {
+          "cproId": 860,
+          "cproCodigo": 579,
+          "caprId": 63,
+          "unidCodigo": 3,
+          "cproCodigoint": "VC000662",
+          "cproCodigobarras": " ",
+          "cproNombre": "TRAMADOL AMPOLLAS 100 MG",
+          "cproDescripcion": "TRAMADOL AMPOLLAS 100 MG",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 861,
+          "cproCodigo": 580,
+          "caprId": 63,
+          "unidCodigo": 3,
+          "cproCodigoint": "VC000645",
+          "cproCodigobarras": " ",
+          "cproNombre": "DIAZEPAM AMPOLLAS X 2 ML",
+          "cproDescripcion": "DIAZEPAM AMPOLLAS X 2 ML",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 862,
+          "cproCodigo": 581,
+          "caprId": 63,
+          "unidCodigo": 25,
+          "cproCodigoint": "VC000787",
+          "cproCodigobarras": " ",
+          "cproNombre": "COLIVET X 50 ML",
+          "cproDescripcion": "COLIVET X 50 ML",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 863,
+          "cproCodigo": 582,
+          "caprId": 63,
+          "unidCodigo": 25,
+          "cproCodigoint": "000110-IMV",
+          "cproCodigobarras": " ",
+          "cproNombre": "DIPIRONA 50 X 100 CC",
+          "cproDescripcion": "DIPIRONA 50 X 100 CC",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 864,
+          "cproCodigo": 583,
+          "caprId": 63,
+          "unidCodigo": 25,
+          "cproCodigoint": "IV0000007",
+          "cproCodigobarras": " ",
+          "cproNombre": "VETALGINA 100 ML",
+          "cproDescripcion": "VETALGINA 100 ML",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 865,
+          "cproCodigo": 584,
+          "caprId": 63,
+          "unidCodigo": 25,
+          "cproCodigoint": "IV0000009",
+          "cproCodigobarras": " ",
+          "cproNombre": "VETALGINA 50 ML",
+          "cproDescripcion": "VETALGINA 50 ML",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 866,
+          "cproCodigo": 585,
+          "caprId": 63,
+          "unidCodigo": 46,
+          "cproCodigoint": "VM010211",
+          "cproCodigobarras": " ",
+          "cproNombre": "CREMAGAN (ANALGESICO TOPICO) POMO 200GR",
+          "cproDescripcion": "CREMAGAN (ANALGESICO TOPICO) POMO 200GR",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 867,
+          "cproCodigo": 586,
+          "caprId": 63,
+          "unidCodigo": 25,
+          "cproCodigoint": "VC000191",
+          "cproCodigobarras": " ",
+          "cproNombre": "DESALGINA FCO X 100ML",
+          "cproDescripcion": "DESALGINA FCO X 100ML",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        },
+        {
+          "cproId": 868,
+          "cproCodigo": 3872,
+          "caprId": 63,
+          "unidCodigo": 3,
+          "cproCodigoint": "VC000811",
+          "cproCodigobarras": " ",
+          "cproNombre": "TRAMADOL AMPOLLAS 2ML (50MG)",
+          "cproDescripcion": "TRAMADOL AMPOLLAS 2ML (50MG)",
+          "cproMarca": "0",
+          "cproUbicacion": "0"
+        }
+      ]
+    }
+  ]
+}
